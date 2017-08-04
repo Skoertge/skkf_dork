473c12079edc26851959ef9f4be6f0272a234da1
kfohrman <30729397+kfohrman@users.noreply.github.com>
2017-08-04 14:06:23 -0500

    Add files via upload

--- src/Battle.java
+++ src/Battle.java
@@ -7,33 +7,35 @@ public class Battle {
 
 	public static void enemies(String location) {
 
-		HashMap<String, Double> enemies = new HashMap<String, Double>();
+		HashMap<String, Integer> enemies = new HashMap<String, Integer>();
 
 		// Populate HashMap with enemies based on location
 		if (location == "cemetery") {
+			// Add rat with HP of 3
+			enemies.put("rat", 8);
 			// Add skeleton with HP of 8
-			enemies.put("skeleton", new Double(8));
+			enemies.put("skeleton", 8);
 			// Add zombie with HP of 12
-			enemies.put("zombie", new Double(12));
+			enemies.put("zombie", 12);
 
 			List<String> keysList = new ArrayList<String>(enemies.keySet());
 			int r = new Random().nextInt(keysList.size());
 			String randomEnemy = keysList.get(r);
-			Double enemyHP = enemies.get(randomEnemy);
+			Integer enemyHP = enemies.get(randomEnemy);
 
 			System.out.println("You have encountered a " + randomEnemy + "!");
 			System.out.println("The " + randomEnemy + " has " + enemyHP + " HP! \nWhat will you do?");
 
 		} else if (location == "cave") {
 			// Add rat with HP of 3
-			enemies.put("rat", new Double(12));
+			enemies.put("rat", 12);
 			// Add bat with HP of 6
-			enemies.put("bat", new Double(16));
+			enemies.put("bat", 16);
 
 			List<String> keysList = new ArrayList<String>(enemies.keySet());
 			int r = new Random().nextInt(keysList.size());
 			String randomEnemy = keysList.get(r);
-			Double enemyHP = enemies.get(randomEnemy);
+			Integer enemyHP = enemies.get(randomEnemy);
 
 			System.out.println("You have encountered a " + randomEnemy + "!");
 			System.out.println("The " + randomEnemy + " has " + enemyHP + " HP! \nWhat will you do?");
