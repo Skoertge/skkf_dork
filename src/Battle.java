c0e5ca433d6a9a56c85502e3471a43da602959ab
kfohrman <30729397+kfohrman@users.noreply.github.com>
2017-08-04 15:05:54 -0500

    Add files via upload

--- src/Battle.java
+++ src/Battle.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Battle {
 
@@ -12,7 +13,7 @@ public class Battle {
 		// Populate HashMap with enemies based on location
 		if (location == "cemetery") {
 			// Add rat with HP of 3
-			enemies.put("rat", 8);
+			enemies.put("rat", 3);
 			// Add skeleton with HP of 8
 			enemies.put("skeleton", 8);
 			// Add zombie with HP of 12
@@ -25,12 +26,15 @@ public class Battle {
 
 			System.out.println("You have encountered a " + randomEnemy + "!");
 			System.out.println("The " + randomEnemy + " has " + enemyHP + " HP! \nWhat will you do?");
+			encounter(location, randomEnemy, enemyHP);
 
 		} else if (location == "cave") {
 			// Add rat with HP of 3
-			enemies.put("rat", 12);
+			enemies.put("rat", 3);
 			// Add bat with HP of 6
-			enemies.put("bat", 16);
+			enemies.put("bat", 6);
+			// Add something with HP of 8
+			enemies.put("beggar", 8);
 
 			List<String> keysList = new ArrayList<String>(enemies.keySet());
 			int r = new Random().nextInt(keysList.size());
@@ -39,10 +43,54 @@ public class Battle {
 
 			System.out.println("You have encountered a " + randomEnemy + "!");
 			System.out.println("The " + randomEnemy + " has " + enemyHP + " HP! \nWhat will you do?");
+			encounter(location, randomEnemy, enemyHP);
 		}
 	}
 
-	public static void encounter(String location, HashMap enemies) {
+	@SuppressWarnings({ "resource", "serial" })
+	private static void encounter(String location, String randomEnemy, Integer enemyHP) {
+		ArrayList<String> options = new ArrayList<String>() {
+			{
+				add("1. Attack");
+				add("2. Defend");
+				add("3. Use Item");
+				add("4. Escape");
+			}
+		};
+
+		System.out.println(options);
+
+		Scanner s = new Scanner(System.in);
+		String choice = s.next();
+
+		if (choice.equalsIgnoreCase("Attack") || (choice == "1")) {
+			attack();
+		} else if (choice.equalsIgnoreCase("Defend") || (choice == "2")) {
+			defend();
+		} else if (choice.equalsIgnoreCase("Use Item") || (choice == "3")) {
+			useItem();
+		} else if (choice.equalsIgnoreCase("Escape") || (choice == "4")) {
+			escape();
+		}
+	}
+
+	private static void attack() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void defend() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void useItem() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void escape() {
+		// TODO Auto-generated method stub
 
 	}
 
